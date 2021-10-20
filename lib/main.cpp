15df8a7424b8d3f0a9242e7711759998e340a79f
ras-drive <rasdrive4165@protonmail.com>
2021-10-20 00:37:33 -0500

    added file I/O

---
+++ lib/main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+#include "notes.h"
+#include "csv.h"
+
+bool saveFolderEmpty = false;
+
+std::vector<std::filesystem::directory_entry> getSaveFolder() {
+    std::string path = canonicalize_file_name(__FILE__);
+    std::vector<std::filesystem::directory_entry> files = listFiles(path);
+    return files;
+}
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnreachableCode"
+void printSaveFolderContents() {
+    std::vector<std::filesystem::directory_entry> files = getSaveFolder();
+    int count = 1;
+    for (const auto &file: files) {
+        std::cout << count << ": " << file.path() << "\n";
+        count++;
+    }
+    if (count == 1) {
+        std::cout << "ERROR: There are no files in the save-data/ folder\n";
+        saveFolderEmpty = true;
+    } else {
+        saveFolderEmpty = false;
+    }
+}
+
+int main() {
+    std::vector<std::string> notes;
+    std::string temp;
+    start();
+
+    while (true) {
+        getline(std::cin, temp);
+        // list notes functionality
+        if (stringCleaner(temp).empty()) {
+        } else if (stringCleaner(temp) == "list") {
+            if (notes.empty()) {
+                std::cout << "You need to add a note to list them!\n";
+            } else {
+                for (auto & note : notes) {
+                    std::cout << note << "\n";
+                }
+            }
+            // remove from list functionality
+        } else if (stringSplitter(stringCleaner(temp))[0] == "remove" &&
+                   stringSplitter(stringCleaner(temp)).size() > 1) {
+            try {
+                int num = std::stoi(stringSplitter(stringCleaner(temp))[1]);
+                if (num > 0 && num <= notes.size()) {
+                    notes.erase(notes.begin() + num - 1);
+                } else if (notes.empty()) {
+                    std::cout << "You need to have a note in the list to remove it!\n";
+                } else if (num > notes.size()) {
+                    std::cout << "You don't even have " << num << " notes in the list!\n";
+                }
+                // catches in case the first word in your note is "remove"
+            } catch (std::invalid_argument&) {
+                notes.push_back(temp);
+            }
+            // quit program functionality
+        } else if (stringCleaner(temp) == "quit" || stringCleaner(temp) == "q") {
+            return 0;
+        } else if (stringCleaner(temp) == "save") {
+            if (!notes.empty()) {
+                std::cout << "What would you like the file to be named?\n";
+                std::string saveFolder;
+                std::filesystem::directory_entry saveFolderPath = getSaveFolder()[0];
+                std::vector<std::string> splitPath = pathSplitter(saveFolderPath.path());
+                splitPath.pop_back();
+                std::string folderAbsPath;
+                for (auto & i : splitPath) {
+                    folderAbsPath += i, folderAbsPath += '/';
+                }
+                folderAbsPath += "saved-notes/";
+
+                std::string fileName;
+                std::getline(std::cin, fileName);
+
+                fileName += ".csv";
+
+                std::string fullFilePath = folderAbsPath + fileName;
+
+                std::ofstream fileOut(fullFilePath);
+
+                if (!fileOut.is_open()) {
+                    std::cout << "ERROR: could not write to file!\n";
+                } else {
+                    fileOut.write("note\n", 5);
+                    for (auto note : notes) {
+                        note += "\n";
+                        std::string str = note;
+                        fileOut << str;
+                    }
+                    std::cout << "Notes list saved as " << fileName << "\n";
+                    fileOut.close();
+                }
+            } else {
+                std::cout << "You need to add some notes to the list before you can save them!\n";
+            }
+        } else if (stringCleaner(temp) == "open") {
+            std::vector<std::filesystem::directory_entry> files = getSaveFolder();
+            printSaveFolderContents();
+            // checks if the save-data/ folder is empty
+            if (saveFolderEmpty) {
+                continue;
+            }
+            int fileChoice;
+            std::cout << "Which file would you like to open?\n";
+
+            std::cin >> fileChoice;
+            std::filesystem::path file = files[fileChoice - 1].path();
+
+            // uses csv.h
+            io::CSVReader<1> in(file);
+
+            std::vector<std::string> tempVec;
+            in.read_header(io::ignore_extra_column, "note");
+            std::string note;
+            while(in.read_row(note)){
+                tempVec.push_back(note);
+            }
+
+
+            // empties notes vector
+            for (std::string nul : notes) {
+                notes.pop_back();
+            }
+
+            // pushes from file to notes vector
+            for (auto & i : tempVec) {
+                notes.push_back(i);
+            }
+        } else {
+            // default statement
+            notes.push_back(temp);
+        }
+    }
+}
--- lib/notes.h
+++ lib/notes.h
@@ -3,6 +3,11 @@
 //
 
 #include <sstream>
+#include <fstream>
+#include <filesystem>
+#include <iterator>
+#include <vector>
+#include <iostream>
 
 #ifndef NOTES_APP_NOTES_H
 #define NOTES_APP_NOTES_H
@@ -17,13 +22,12 @@ static std::string stringCleaner(std::string &str) {
 }
 
 // function to split the strings for the remove functionality
-static std::vector<std::string> stringSplitter(const std::string& str) {
-    std::string word;
+static std::vector<std::string> stringSplitter(const std::string &str) {
     std::istringstream iss{str};
 
     std::vector<std::string> words{
-        std::istream_iterator<std::string>{iss},
-        std::istream_iterator<std::string>{}
+            std::istream_iterator<std::string>{iss},
+            std::istream_iterator<std::string>{}
     };
     return words;
 }
@@ -34,7 +38,46 @@ static void start() {
                  "start typing to take a note.\n"
                  "type \"list\" to display your notes.\n"
                  "typing \"remove\" followed by the items position removes it from the list.\n"
+                 "typing \"save\" will save to a file with the name of your choice.\n"
+                 "typing \"open\" will list txt files to open, you can open by number.\n"
                  "Type \"quit\" or \"q\" to finish taking notes.\n\n";
 }
+
+
+
+static std::vector<std::string> pathSplitter(const std::string &str) {
+    std::vector<std::string> pathVec;
+    std::string s = str;
+    std::string delimiter = "/";
+
+    size_t pos;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        pathVec.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    return pathVec;
+}
+
+static std::vector<std::filesystem::directory_entry> listFiles(std::string &filePath) {
+    std::vector<std::string> pathVec = pathSplitter(filePath);
+
+    // rebuilds path from string down to the directory
+    std::string folderPath;
+    for (auto & i : pathVec) {
+        folderPath += i + "/";
+    }
+    // default name for saved notes directory
+    std::string saveFolderPath = "saved-notes/";
+    saveFolderPath = folderPath + saveFolderPath;
+
+    std::vector<std::filesystem::directory_entry> files;
+    for (const auto &entry : std::filesystem::directory_iterator(saveFolderPath)) {
+        files.push_back(entry);
+    }
+    return files;
+}
+
 
 #endif //NOTES_APP_NOTES_H
